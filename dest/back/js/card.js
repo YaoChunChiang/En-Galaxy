d13eac32f98e812bffb5f2016e853bbb817d4491
HorseChun <t159753g@gmail.com>
2019-09-10 10:13:51 +0800

    add back card

--- dest/back/js/card.js
+++ dest/back/js/card.js
@@ -12,6 +12,7 @@ function init(){
 
 
         if(e.target.innerText === '新增字卡'){
+            console.log("???")
             e.target.innerText = '送出';
             e.target.nextElementSibling.classList.remove('d-none');
             let input = document.createElement('input');
@@ -23,29 +24,54 @@ function init(){
 
         }else if(e.target.innerText == '送出'){
             // 送出的code寫這
-            let newVocab = e.target.parentNode.previousElementSibling.firstElementChild.lastElementChild.value;
-            let level = 0;
-            if(e.target.id === 'basicVocabAdd'){
-                level = 1;
-            }else if(e.target.id === 'midVocabAdd'){
-                level = 2
-                // console.log(2);
-            }else if(e.target.id === 'highVocabAdd'){
-                level = 3
+            // console.log(e.target.parentNode.previousElementSibling.firstElementChild.lastElementChild.value)
+            //確定input裡面有填資料
+            if(e.target.parentNode.previousElementSibling.firstElementChild.lastElementChild.value != ''){
+                let newVocab = e.target.parentNode.previousElementSibling.firstElementChild.lastElementChild.value;
+                let level = 0;
+                if(e.target.id === 'basicVocabAdd'){
+                    level = 1;
+                }else if(e.target.id === 'midVocabAdd'){
+                    level = 2
+                    // console.log(2);
+                }else if(e.target.id === 'highVocabAdd'){
+                    level = 3
+                }
+                console.log(level);
+                
+                
+                e.target.parentNode.previousElementSibling.firstElementChild.lastElementChild.remove();
+                // $.ajax({
+                    //     url: "defaultCardUpdate.php",
+                    //     data: {vocab: newVocab, lev: level},
+                    //     type: "GET",
+                    //     // success: function(data){
+                        //     //     $('#feedback').html('<h1>' + data + '</h1>');
+                        //     // }
+                        // });
+                        //做假的
+                let newVocabTemp = `${newVocab}
+                <a class="btn btn-danger float-right" href="#">
+                <i class="fa fa-trash-o">
+                </i>
+                </a>
+                `;
+                let newVocabContainer = document.createElement('button');
+                newVocabContainer.setAttribute('class', 'list-group-item list-group-item-action');
+                newVocabContainer.setAttribute('type', 'button');
+                newVocabContainer.innerHTML = newVocabTemp;
+                // console.log(newVocabContainer);
+                e.target.parentNode.previousElementSibling.firstElementChild.appendChild(newVocabContainer);
+                        
+                e.target.innerText = "新增字卡";
+                e.target.nextElementSibling.classList.add('d-none');
+                //送去資料庫
+                $.get("defaultCardUpdate.php", {vocab: newVocab, lev: level})
+            }else{// No input
+                e.target.parentNode.previousElementSibling.firstElementChild.lastElementChild.setAttribute('placeholder', '未輸入資料');
             }
-            console.log(level);
-
-            // $.ajax({
-            //     url: "defaultCardUpdate.php",
-            //     data: {vocab: newVocab, lev: level},
-            //     type: "GET",
-            //     // success: function(data){
-            //     //     $('#feedback').html('<h1>' + data + '</h1>');
-            //     // }
-            // });
-
-
-            $.get("defaultCardUpdate.php", {vocab: newVocab, lev: level})
+            
+            
         }
 
         //建立取消鈕的事件聆聽功能
