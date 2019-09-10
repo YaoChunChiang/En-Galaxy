28b8e5e69e816600c1dfa77e347ae8d6ca4053f5
jovialhsu <celia0528@gmail.com>
2019-09-10 20:48:22 +0800

    display question

--- dest/js/eventLightBox.js
+++ dest/js/eventLightBox.js
@@ -6,7 +6,6 @@ $(document).ready(function () {
         $('.closeShow').slideToggle();
     })
 
-
     $('.showLaunchBg').click(function () {
         $('#showLaunch').slideToggle();
         $('.showLaunchBg').slideToggle();
@@ -24,38 +23,31 @@ $(document).ready(function () {
         $('.showLaunchBg').slideToggle();
         $('.closeShow').slideToggle();
     })
+    $('.askQuestion').click(function(){ 
+       $('#forumQAddWindow').slideToggle();
+       $('.close').slideToggle();
+      
+       
+    })
+    $('.close').click(function(){  
+        $('#forumQAddWindow').slideToggle();
+        $('.askQuestion').slideToggle();
+       
+       
+     })
+     $('#forumQAddWindow').click(function(){
+        $('.askQuestion').slideToggle();
+        
+        
+     })
+     $('.window').click(function(){ 
+        $('#forumQAddWindow').slideToggle();
+        $('.askQuestion').slideToggle();
+        $('.close').slideToggle();
+       
+     })
+ 
+
+    
 });
-    // //改變活動名稱
-    // $('#createName').change(function(){
-    //     var name = $(this).val();
-    //     $("#user-evt-name").val(name);
-    // });
     
-    // $('.hold-act').change(function () {
-    //     var name = $(this).val();
-    //     $("#user-evt-name").val(name);
-    // });
-    
-
-    //首頁表單改變時，燈箱的表單也跟著改變
-//     function assignChange(eventNode1, eventNode2) {
-//         $(eventNode1).change(function () {
-//             // console.log($(this).children('option:selected').index());
-//             var ind = $(this).children('option:selected').index();
-//             ind = parseInt(ind) + 1;
-//             $(eventNode2).children(':nth-child(n)').attr('selected', false);
-//             $(eventNode2).children(':nth-child(' + ind + ')').attr('selected', true);
-//         });
-//     }
-//     assignChange('#createType', '#boxType');//改變類型
-//     assignChange('#createPeople', '#boxPeople');//改變店鋪
-//     assignChange('#createStore', '#boxStore');//改變人數
-
-//     if (document.querySelector('.mobile-create')){
-//         $('.mobile-create').click(function(){
-//                 $('#show-launch').slideToggle();
-//                 $('.show-launch-bg').slideToggle();
-//                 $('.close-show-launch').slideToggle();
-//         });
-//     }
-// });
