af70b7a43dcac1d5bef838d0729ca4761db475bc
whnliao <whnliao@gmail.com>
2019-09-12 21:52:29 +0800

    back admin/account start

---
+++ dev/back/js/administrator.js
@@ -0,0 +1,14 @@
+function init(){
+    //撈json顯示
+}
+function addAdmin(){
+    //新增管理員 ajax
+}
+function editAdmin(){
+    //編輯管理員"帳號","密碼" ajax
+}
+function delAdmin(){
+    //刪除管理員身分
+}
+
+window.addEventListener("load", init, false);
