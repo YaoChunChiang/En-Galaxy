b732d645a26e4816cfec1610f2516e4dedd5dc42
Alex <t159753g@gmail.com>
2019-09-13 21:58:09 +0800

    中秋節撈到單字的資料庫

--- dev/card.php
+++ dev/card.php
@@ -2,18 +2,38 @@
 
 $erroMsg = "";
 try{
-    $dsn = "mysql:host=localhost;port=3306;dbname=dd102g4_;charset=utf8";
+    $dsn = "mysql:host=localhost;port=8889;dbname=dd102g4_;charset=utf8";
     $user = "root";
-    $password = "MynameisAlex";
+    $password = "root";
     $options = array(PDO::ATTR_CASE=>PDO::CASE_NATURAL, PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
     $pdo = new PDO($dsn, $user, $password,$options);
 
 
-    $getVocabClass = "SELECT DISTINCT b.card_class FROM `vocab` `a`, `card_class` `b` WHERE a.card_class = b.card_no and b.mem_no = 1";
-    $statement = $pdo->prepare($getVocabClass);
-    // $statement -> bindValue(":vlevel", $level);
-    // $statement -> bindValue(":newVocab", $newVocab);
-    $statement -> execute();
+    $doWhat = $_GET['who'];
+    $memNum = 1;
+
+
+
+    if($doWhat == 'start'){
+        // $sql = "SELECT a.card_class, b.vocab FROM card_class a, vocab b WHERE a.card_no = b.card_class and mem_no = $memNum";
+        $sql = "SELECT a.card_class, b.vocab FROM card_class a, vocab b WHERE a.card_no = b.card_class and mem_no = $memNum";
+        $vocabs = $pdo->query($sql);
+        // $vocabsObject = $vocabs->fetchObject();
+        // $test = '';
+        // while($vocabsObject = $vocabs->fetchObject()){
+        //     $test += json_encode($vocabsObject)
+        // }
+        $vocabsObject = $vocabs->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($vocabsObject);
+        // echo $vocabsObject;
+    }
+
+
+    // $getVocabClass = "SELECT DISTINCT b.card_class FROM `vocab` `a`, `card_class` `b` WHERE a.card_class = b.card_no and b.mem_no = 1";
+    // $statement = $pdo->prepare($getVocabClass);
+    // // $statement -> bindValue(":vlevel", $level);
+    // // $statement -> bindValue(":newVocab", $newVocab);
+    // $statement -> execute();
   
     }catch(PDOException $e){
         $erroMsg = $erroMsg . "錯誤訊息: " . $e->getMessage() . "<br>";
