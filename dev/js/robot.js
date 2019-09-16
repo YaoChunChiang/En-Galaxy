2d22fc3294057f508e6907aabde36eeb750041c3
rubylin0328 <rubylin0328@gmail.com>
2019-09-16 22:02:57 +0800

    almost done

--- dev/js/robot.js
+++ dev/js/robot.js
@@ -1,4 +1,23 @@
 $(document).ready(function () {
+    let storage = sessionStorage;
+    if(storage.getItem('mem_no') != null){
+        let setNo = storage.getItem('set_no');
+        let setColor = storage.getItem('set_color');
+        $.ajax({    
+            url: `robot.php?action=loadMemRole`,
+            data: {
+                setNo:setNo 
+            },
+            type: 'GET',
+            success: function(memRoleRow){
+                let memRole = JSON.parse(memRoleRow);
+                $('#memRoleBody').attr('src',memRole[0].set_body_src).css('filter',`hue-rotate(${setColor}deg)`);
+                $('#memRolePart').attr('src',memRole[0].set_part_src);
+                $('#memRoleLeftHand').attr('src',memRole[0].set_lefthand_src);
+                $('#memRoleRightHand').attr('src',memRole[0].set_righthand_src);
+            }
+        });
+    }
     $.ajax({    
         url: `robot.php?action=load`,
         data: {
@@ -6,26 +25,11 @@ $(document).ready(function () {
         },
         type: 'GET',
         success: function(qnasRows){
-            let qnas = JSON.parse(qnasRows);
+            qnas = JSON.parse(qnasRows);
             for(let i=0; i<qnas.length; i++){
                 let htmlStr = "";
-                htmlStr += `<div class="col-lg-4">`;
-                htmlStr += `<div class="card" id="${qnas[i].keyword_no}">`;
-                htmlStr += `<div class="card-header">常見問答${qnas[i].keyword_no}</div>`;
-                htmlStr += `<div class="card-body">`;
-                htmlStr += `<form action="" method="get" enctype="multipart/form-data">`;
-                htmlStr += `<table class="table table-responsive-sm table-sm">`;
-                htmlStr += `<tr><th>常見問題</th><td><input class="form-control qnaQ" type="text" value="${qnas[i].keyword_con}"></td></tr>`;
-                htmlStr += `<tr><th>常見問題的回答</th><td><textarea class="form-control qnaA" rows="3" value="">${qnas[i].keyword_ans}</textarea></td></tr>`;
-                if(qnas[i].keyword_status == 0){
-                    htmlStr += `<tr><th>常見問答上架狀態</th><td><label class="switch switch-3d switch-success"><input class="switch-input qnaStatus" type="checkbox"><span class="switch-slider"></span></label></td></tr>`;
-                }else{
-                    htmlStr += `<tr><th>常見問答上架狀態</th><td><label class="switch switch-3d switch-success"><input class="switch-input qnaStatus" type="checkbox" checked="checked"><span class="switch-slider"></span></label></td></tr>`;
-                }
-                htmlStr += `</table>`;
-                htmlStr += `<div class="row"><div class="col-6 col-sm-4 col-md-2 col-xl mb-3 mb-xl-0"><button class="btn btn-pill btn-block btn-success confirmModify" type="button">確認修改</button></div><div class="col-6 col-sm-4 col-md-2 col-xl mb-3 mb-xl-0"><button class="btn btn-pill btn-block btn-danger delete" type="button">刪除</button></div></div>`;
-                htmlStr += `</form></div></div></div>`;
-                $('.qnasRow').append(htmlStr);
+                htmlStr += `<div id="qOpt${i}" class="qOpt">${qnas[i].keyword_con}</div>`;
+                $('.qOpts').append(htmlStr);
             }
         }
     });
@@ -65,6 +69,13 @@ $(document).ready(function () {
         $('.conversationSlider').css('display', 'none');
         $('.conversationSlider').css('left', '-100%');
         $('.conversationQ').css('display', 'block');
+        $('.optChosen').text('');
+        $('.answerWithOpt').text('');
+        $('.wordInput').text('');
+        $('.resultWithInput').text('');
+        $('.addToCard').css('display', 'none');
+        $('.robotCardClasses').remove();
+        $('.addToCard').text('加入字卡');
     });
     $('.translateVocabulary').click(function () {
         $('.conversationSlider').css('left', '0');
@@ -73,7 +84,9 @@ $(document).ready(function () {
         $('.conversationSlider').css('left', '-100%');
         $('.wordInput').text('');
         $('.resultWithInput').text('');
-        $('.addToCard').css('display', 'none')
+        $('.addToCard').css('display', 'none');
+        $('.robotCardClasses').remove();
+        $('.addToCard').text('加入字卡');
     });
     $('.translateBtn').click(function () {
         if (/^[A-Za-z]+$/.test($('.inputEnglish').val()) == true && $('.inputEnglish').val() != '') {
@@ -110,7 +123,8 @@ $(document).ready(function () {
         }else{
            $('.resultWithInput').text(' '); 
         }
-        
+        $('.robotCardClasses').remove();
+        $('.addToCard').text('加入字卡');
     })
     $('.qNA').click(function () {
         $('.conversationSlider').css('left', '-200%');
@@ -120,16 +134,63 @@ $(document).ready(function () {
         $('.optChosen').text('');
         $('.answerWithOpt').text('');
     });
-    $('.qOpt1').click(function () {
-        $('.optChosen').text($('.qOpt1').text());
-        $('.answerWithOpt').text('鄧紫棋，又名G.E.M.，本名鄧詩穎，是一名香港創作歌手。');
+    // $('.qOpt1').click(function () {
+    //     $('.optChosen').text($('.qOpt1').text());
+    //     $('.answerWithOpt').text('鄧紫棋，又名G.E.M.，本名鄧詩穎，是一名香港創作歌手。');
+    // });
+    // $('.qOpt2').click(function () {
+    //     $('.optChosen').text($('.qOpt2').text());
+    //     $('.answerWithOpt').text('快速動眼期是動物睡眠的一個階段，又稱快速動眼睡眠。');
+    // });
+    // $('.qOpt3').click(function () {
+    //     $('.optChosen').text($('.qOpt3').text());
+    //     $('.answerWithOpt').text('阿部瑪利亞是日本偶像藝人，為女子偶像團體AKB48 Team TP成員，神奈川縣出身，所屬經紀公司為日本音樂娛樂。');
+    // });
+    $('.qOpts').on('click','.qOpt', function(){
+        let qnaNo = $(this).attr('id').charAt(4);
+        $('.optChosen').text($(this).text());
+        $('.answerWithOpt').text(qnas[qnaNo].keyword_ans);
     });
-    $('.qOpt2').click(function () {
-        $('.optChosen').text($('.qOpt2').text());
-        $('.answerWithOpt').text('快速動眼期是動物睡眠的一個階段，又稱快速動眼睡眠。');
-    });
-    $('.qOpt3').click(function () {
-        $('.optChosen').text($('.qOpt3').text());
-        $('.answerWithOpt').text('阿部瑪利亞是日本偶像藝人，為女子偶像團體AKB48 Team TP成員，神奈川縣出身，所屬經紀公司為日本音樂娛樂。');
+    $('.addToCard').click(function(){
+        if(storage.getItem('mem_no') == null){
+            alert('請登入!!!')
+            $('#loginBox').css('display', 'block');
+        }else if($('.addToCard').text() == '加入字卡'){
+            let memNo = storage.getItem('mem_no');
+            $.ajax({    
+                url: `robot.php?action=addToCard`,
+                data: {
+                    memNo:memNo
+                },
+                type: 'GET',
+                success: function(cardClassesRows){
+                    cardClasses = JSON.parse(cardClassesRows);
+                    let htmlStr = "";
+                    htmlStr += `
+                    <div class="robotCardClasses"><label for="cardClasses">字卡類別</label>`;
+                    htmlStr += `<select id="cardClasses">`;
+                    for(let i=0; i<cardClasses.length; i++){
+                        htmlStr += `<option value="${cardClasses[i].mem_no}">${cardClasses[i].card_class}</option>` 
+                    }
+                    htmlStr += `</select></div>`;
+                    $(htmlStr).insertBefore('.addToCard');
+                    $('.addToCard').text('確定加入');
+                }
+            });
+        }else{
+            let cardClass = $('#cardClasses').val();
+            let cardVocabulary = $('.wordInput').text();
+            $.ajax({    
+                url: `robot.php?action=comfirmAdd`,
+                data: {
+                    cardClass:cardClass,
+                    cardVocabulary:cardVocabulary
+                },
+                type: 'GET',
+                success: function(){
+                    
+                }
+            });
+        }
     });
 });
--- dev/robot.php
+++ dev/robot.php
@@ -14,6 +14,38 @@ try{
             $qnasRows = $qnas->fetchAll();
             echo json_encode($qnasRows);
         }
+    }else if($action == "addToCard"){
+        $memNo = $_GET["memNo"];
+        $sql = "select * from card_class where mem_no = :memNo";
+        $cardClasses = $pdo->prepare($sql);
+        $cardClasses->bindValue(":memNo", $memNo);
+        $cardClasses->execute();
+        if( $cardClasses->rowCount() == 0 ){
+            echo "{}";
+        }else{
+            $cardClassesRows = $cardClasses->fetchAll();
+            echo json_encode($cardClassesRows);
+        }
+    }else if($action == "comfirmAdd"){
+        $cardClass = $_GET["cardClass"];
+        $cardVocabulary = $_GET["cardVocabulary"];
+        $sql = "insert into vocab (card_class,vocab) values (:cardClass,:cardVocabulary)";
+        $vocabularyAdded = $pdo->prepare($sql);
+        $vocabularyAdded->bindValue(":cardClass", $cardClass);
+        $vocabularyAdded->bindValue(":cardVocabulary", $cardVocabulary);
+        $vocabularyAdded->execute();
+    }else if($action == "loadMemRole"){
+        $setNo = $_GET["setNo"];
+        $sql = "select * from role_set where set_no = :setNo";
+        $memRole = $pdo->prepare($sql);
+        $memRole->bindValue(":setNo", $setNo);
+        $memRole->execute();
+        if( $memRole->rowCount() == 0 ){
+            echo "{}";
+        }else{
+            $memRoleRow = $memRole->fetchAll();
+            echo json_encode($memRoleRow);
+        }
     }
 }catch(PDOException $e){
     echo $e->getMessage();
