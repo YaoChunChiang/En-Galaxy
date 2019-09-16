03114049456b4cdc3010bc12b025ddd6b87f714c
alex1290 <s101242006@mail1.ncnu.edu.tw>
2019-09-16 17:40:00 +0800

    login role

--- dev/js/login.js
+++ dev/js/login.js
@@ -1,13 +1,52 @@
 function loginInit() {
     //cookie檢查有沒有登入
     let storage = sessionStorage;
-    console.log(storage.getItem('mem_name'));
-    if (storage.getItem('mem_name') != null) {
-        $('.memAfterLogin').css({
-            'display': 'block'
-        });
-        $('#memStatusLogin').text(`登出`);
-        $('#memStatusId').text(storage.getItem('mem_name') + '  您好!');
+    let date = new Date;
+    let today = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+    function loginCheck() {
+        if (storage.getItem('mem_name') != null) {
+            $('.memAfterLogin').css({
+                'display': 'block'
+            });
+            $('#memStatusLogin').text(`登出`);
+            $('#memStatusId').text(storage.getItem('mem_name') + '  您好!');
+            getInfo();
+        } else {
+            storage.clear();
+        }
+    }
+    loginCheck();
+    //連續登入檢查
+    function dateCheck(date){
+        if (today != storage['mem_last_lgn']){
+            $.ajax({
+                url: 'login.php',
+                dataType: 'text',
+                data: {
+                    type: 'dateCheck',
+                    today: today,
+                    memNo: storage['mem_no']
+                },
+                type: 'POST',
+                success: function (response) {
+                    console.log(response);
+                    // memIdRow = JSON.parse(response);
+                    // $.each(memIdRow, function (i, n) {
+                    //     if (n['mem_id'] == $('#mem_id').val()) {
+                    //         alert('帳號已被使用!')
+                    //     } else {
+                    //         $('#memIdCheck').css({
+                    //             'color': 'green',
+                    //             'borderColor': 'green'
+                    //         }).val('可以使用!');
+                    //     }
+                    // });
+                },
+                error: function () {
+                    console.log('沒連資料庫啦')
+                }
+            })
+        }
     }
     //顯示燈箱或登出
     $('#memStatusLogin').click(function () {
@@ -29,6 +68,7 @@ function loginInit() {
     //關閉燈箱
     $('#loginBoxClose').click(function () {
         $('#loginBox').css('display', 'none');
+        storage.clear();
     })
     //顯示&隱藏重填
     $('.loginInfo input').focus(function () {
@@ -49,27 +89,34 @@ function loginInit() {
         $(this).prev().val('');
     })
     //登入更改會員資訊
-    $('#submitBtn').click(function () {
+    function getInfo(){
+        let memId = $('#memId').val();
+        let memPsw = $('#memPsw').val();
+        if (storage.getItem('mem_name') != null){
+            memId = storage.getItem('mem_id');
+            memPsw = storage.getItem('mem_psw');
+        };
+        
         $.ajax({
             url: 'login.php',
             dataType: 'text',
             data: {
-                type: 'login',
-                memId: $('#memId').val(),
-                memPsw: $('#memPsw').val()
+                type: 'getInfo',
+                memId: memId,
+                memPsw: memPsw
             },
             type: 'POST',
             success: function (response) {
+                console.log(memId, memPsw);
                 if (response == 0) {
                     window.alert('帳密錯誤，請重新輸入!')
                 } else {
                     mem = JSON.parse(response);
                     // console.log(mem,mem[0].mem_id);
                     for (const key in mem[0]) {
-                        
                         storage.setItem(key, mem[0][key]);
                     }
-                    window.alert(mem[0]['mem_name'] + '，您好!')
+                    
                     $('#memStatusId').text(`${mem[0]['mem_name']} 您好!`);
                     $('#memStatusGEM').text(mem[0]['mem_money']);
                     $('.memAfterLogin').css({
@@ -80,16 +127,22 @@ function loginInit() {
                     $('#loginBox').css({
                         'display': 'none'
                     })
+                    if ($('#memId').val()!=''){
+                        window.alert(storage['mem_name'] + '，您好!')
+                    }
                     $('.loginInfo input').val('');
-
-
+                    
                 }
-
             },
             error: function () {
-                console.log($('#memId').val())
+                alert('系統異常')
             }
         });
+    }
+    $('#submitBtn').click(function () {
+        
+        getInfo();
+        // dateCheck();
     });
     //註冊頁面
     $('#registeredBtn').click(function () {
@@ -97,7 +150,7 @@ function loginInit() {
         $('#loginBox .roleCreate').css('display', 'block');
     })
     //驗證註冊資訊
-    $('#mem_id').change(function(){
+    $('#mem_id').change(function () {
         $('#memIdCheck').css({
             'color': '#38227c',
             'borderColor': '#ccc'
@@ -116,29 +169,23 @@ function loginInit() {
                 type: 'POST',
                 success: function (response) {
                     memIdRow = JSON.parse(response);
-                    $.each(memIdRow,function(i,n){
-                        if (n['mem_id'] == $('#mem_id').val()){
+                    $.each(memIdRow, function (i, n) {
+                        if (n['mem_id'] == $('#mem_id').val()) {
                             alert('帳號已被使用!')
-                        }else{
+                        } else {
                             $('#memIdCheck').css({
                                 'color': 'green',
-                                'borderColor':'green'
+                                'borderColor': 'green'
                             }).val('可以使用!');
                         }
-                    })
-                    // if (response == true) {
-                    //     window.alert('帳號已被使用!')
-                    // } else {
-                    //     $('#memIdCheck').css('color', 'green').text('可以使用!')
-                    // }
+                    });
                 },
                 error: function () {
                     console.log('沒連資料庫啦')
                 }
             });
-        }
-
-    })
+        };
+    });
     $('#registerSubmit').click(function () {
         console.log(/^\w+@{1}\w+.\w+.\w*/.test($('#mem_email').val()))
         if ($('#memIdCheck').val() != '可以使用!') {
@@ -158,6 +205,32 @@ function loginInit() {
                 let info = $('.registerInfo input[type=text]').not('#pswChecked')[i]
                 storage.setItem(info.getAttribute('id'), info.value)
             }
+            storage.setItem('level_no', '1');
+            storage.setItem('mem_money', '100');
+            storage.setItem('mem_status', '1');
+            storage.setItem('mem_continue', '1');
+            storage.setItem('mem_last_lgn', today);
+            let memData = new Object();
+            for (let i = 0; i < storage.length; i++) {
+                memData[storage.key(i)] = storage.getItem(storage.key(i));
+            }
+            $.ajax({
+                url: 'login.php',
+                dataType: 'text',
+                data: {
+                    type: 'registered',
+                    data: memData
+                },
+                type: 'POST',
+                success: function (response) {
+                    alert('註冊成功!\n' + storage.getItem('mem_name') + '您好!');
+                    loginCheck();
+                },
+                error: function () {
+                    alert('系統異常');
+                }
+            })
+            
         }
 
 
