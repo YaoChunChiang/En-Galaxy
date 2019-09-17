db652ecf4dd5b1a71322132abd27611031264133
HorseChun <t159753g@gmail.com>
2019-09-17 17:32:50 +0800

    aaaHHHHHHHHHHHHHHH

--- dev/back/template/studyEngQuizContent.php
+++ dev/back/template/studyEngQuizContent.php
@@ -1,12 +1,12 @@
 <?php
 $errMsg = "";
 try {
-    $dsn = "mysql:host=localhost;port=3306;dbname=dd102g4;charset=utf8";
-    $user = "root";
-    $password = "root123";
-    $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE => PDO::CASE_NATURAL);
-    $pdo = new PDO($dsn, $user, $password, $options);
-
+    // $dsn = "mysql:host=localhost;port=3306;dbname=dd102g4;charset=utf8";
+    // $user = "root";
+    // $password = "root123";
+    // $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE => PDO::CASE_NATURAL);
+    // $pdo = new PDO($dsn, $user, $password, $options);
+    require_once("../pdoData.php");
     $sql = "select * from video_qs";
     $videosQS = $pdo->query($sql);
     $videosQS->execute();
@@ -16,13 +16,22 @@ try {
 }
 ?>
 
+
+
+
+
+
+
+
+
+
 <div class="card">
     <div class="card-header">
         <div class="float-left"><i class="icon-book-open align-justify"></i>&nbsp;&nbsp;學習影片題庫管理</div>
         <div class="clearfix"></div>
-        <div class="d-inline-block mt-2 float-right"><button class="btn btn-warning mr-1"
-                type="button">上傳題庫</button><button class="btn btn-danger" type="button">取消</button></div>
-
+        <div class="d-inline-block mt-2 float-right">
+            <button class="btn btn-warning mr-1" id="videoQAdd" type="button">上傳題庫</button>
+        </div>
     </div>
 
     <div class="card-body">
@@ -86,10 +95,64 @@ try {
             </li>
         </ul>
 
-        <div class="d-inline-block float-right">
+        <!-- <div class="d-inline-block float-right">
             <button class="btn btn-primary btn-lg" type="button">確認</button>
             <button class="btn  btn-danger btn-lg" type="button">取消</button>
-        </div>
+        </div> -->
 
     </div>
+    
 </div>
+
+
+
+<div class="card position-absolute fixed-top col-lg-4 offset-lg-4 col-md-6 offset-md-3 mt-5 d-none videoQustAdd" style="">
+        <div class="card-header">
+            新增影片資料
+        </div>
+        <div class="card-body">
+            <form>
+                <div class="form-row">
+                    <div class="form-group ml-auto mr-auto col-md-6 col-sm-8">
+                        <label for="videoNumber">影片編號</label>
+                        <select class="form-control" id="videoNumber">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+
+                        </select>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="videoTitle">影片題目</label>
+                        <input type="text" class="form-control" value="ex: Do you like me?">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="opt1">選項一</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="opt2">選項二</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="opt3">選項三</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="opt4">選項四</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="rightAns">正確解答</label>
+                        <input type="text" class="form-control">
+                    </div>
+
+        </div></form>
+        <div class="d-flex flex-row-reverse">
+            <button class="btn btn-primary order-2 mr-1 videoConfirm" type="button">確認</button>
+            <button class="btn btn-danger order-1 cancleUpload" type="button">取消</button>
+        </div>
+    </div>
+</div>
+
+
