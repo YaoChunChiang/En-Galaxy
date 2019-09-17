f418f4cfac375b552f3dd99ca41bf447c7495830
jovialhsu <celia0528@gmail.com>
2019-09-17 15:57:57 +0800

    add myQnA page

--- dev/connectionHsin.php
+++ dev/connectionHsin.php
@@ -1,7 +1,7 @@
 <?php
-$dsn = "mysql:host=localhost;port=8889;dbname=dd102g4_test;charset=utf8";
+$dsn = "mysql:host=localhost;port=3306;dbname=dd102g4_test;charset=utf8";
     $user = "root";
-    $password = "root";
+    $password = "123456/";
     $options=array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE=>PDO::CASE_NATURAL);
     $pdo = new PDO($dsn, $user, $password, $options);
  ?>
