ec6c3aef48638b3b56f5262e5aa463e56c7bb2a9
HorseChun <t159753g@gmail.com>
2019-09-17 15:09:28 +0800

    videoList +

--- dev/js/video.js
+++ dev/js/video.js
@@ -1,11 +1,49 @@
+let memNum = sessionStorage['mem_no'] ? sessionStorage['mem_no'] : 'notMem';
+let memLevel = sessionStorage['level_no'] ? sessionStorage['level_no'] : 'notMem';
+console.log(memNum,',',memLevel);
 function init(){
-    let memNum = sessionStorage['mem_no'] ? sessionStorage['mem_no'] : 'notMem';
-    let memLevel = sessionStorage['level_no'] ? sessionStorage['level_no'] : 'notMem';
-    console.log(memNum,',',memLevel);
 
 
     $.get('videoList.php', {who: 'start'}, data => {makeVideoList(JSON.parse(data))});
+
+
+
+
+
+    function levelSelector(){
+        if($(this).hasClass('tagRed')){
+            $('.mid').css('display', 'none');
+            $('.high').css('display', 'none');
+            $('.basic').fadeIn(200)
+        }else if($(this).hasClass('tagBlue')){
+            $('.basic').css('display', 'none');
+            $('.high').css('display', 'none');
+            $('.mid').fadeIn(200)
+        }else if($(this).hasClass('tagGreen')){
+            $('.basic').css('display', 'none');
+            $('.mid').css('display', 'none');
+            $('.high').fadeIn(200)
+        }else{
+            $('.basic').fadeIn(200);
+            $('.mid').fadeIn(200);
+            $('.high').fadeIn(200);
+        }
+    }
+
+
+    $('.tagRed').click(levelSelector);
+    $('.tagBlue').click(levelSelector);
+    $('.tagGreen').click(levelSelector);
+    $('.tagYellow').click(levelSelector);
 }
+
+function connotWatch(){
+    alert('您沒有權限看此部影片，請加入會員或進行遊戲成為高級會員');
+    if(memNum == 'notMem'){
+        $('#loginBox').css('display', 'block');
+    }
+}
+
 let makeVideoList = (videosArr) =>{
     console.log(videosArr);
     let newsVideos = videosArr.filter(obj => obj['video_type'] === '新聞');
@@ -19,25 +57,31 @@ let makeVideoList = (videosArr) =>{
     //動態新增影片清單
     videosArr.forEach(obj => {
         let level = '';
+        let levelEng = '';
         switch(obj['level_no']){
             case '1':
                 level = "初級";
+                levelEng = 'basic';
             break;
             case '2':
                 level = "中級";
+                levelEng = 'mid';
             break;
             case '3':
                 level = "高級";
+                levelEng = 'high';
             break;
         }
         let divVideoFrame = document.createElement('div');
         let a = document.createElement('a');
         let divVideoImg = document.createElement('div');
-        let content = `<img src="img/video/video.png" alt="我是影片">
+        let content = `<img src="img/video/${obj['video_pic']}" alt="我是影片">
                         <p>${obj['video_name']}</p>
                         <div class="video_tag">${level}</div>
                         `;
-        divVideoFrame.setAttribute('class', 'videoFrame');
+        divVideoFrame.setAttribute('class', 'videoFrame videoToBig ' + levelEng);
+        // divVideoFrame.setAttribute('class', levelEng);
+        
         a.setAttribute('href', 'video.html');
         divVideoImg.setAttribute('class', 'videoImg');
 
@@ -58,6 +102,32 @@ let makeVideoList = (videosArr) =>{
         }
     });//動態新增影片清單結束        
     console.log(newsVideos ,",", musicVideos,"," , showVideos);
+
+    //判斷是否有資格看中高級影片
+    switch(memLevel){
+        case 'notMem':
+        case '1':
+            $('.mid').children('a').attr('href', 'javascript:;');
+            $('.high').children('a').attr('href', 'javascript:;');
+            $('.mid').addClass('videoToBlur');
+            $('.high').addClass('videoToBlur');
+            $('.mid').removeClass('videoToBig');
+            $('.high').removeClass('videoToBig');
+            $('.mid').click(connotWatch);
+            $('.high').click(connotWatch);
+        break;
+        //     $('.mid').children('a').attr('href', 'javascript:;');
+        //     $('.high').children('a').attr('href', 'javascript:;');
+        //     $('.mid').click(connotWatch);
+        //     $('.high').click(connotWatch);
+        // break;
+        case '2':
+            $('.high').children('a').attr('href', 'javascript:;');
+            $('.high').addClass('videoToBlur');
+            $('.high').removeClass('videoToBig');
+            $('.high').click(connotWatch);
+        break;
+    }
 }
     // <div class="videoFrame">
     //     <a href="video.html">
