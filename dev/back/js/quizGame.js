4184ae9a89b50d33dd21157eb9819bc7d0ccf0b4
alex1290 <s101242006@mail1.ncnu.edu.tw>
2019-09-19 22:27:59 +0800

    quizZZZZZZZZZZZZZZZZZZZZZ fix

--- dev/back/js/quizGame.js
+++ dev/back/js/quizGame.js
@@ -1,25 +1,109 @@
+function quizGameInit() {
+    let storage = sessionStorage;
+    if (storage['gameQuizPage'] == null) {
+        storage.setItem('gameQuizPage', 1);
+    }
+    let questionPage = storage['gameQuizPage'];
+    let questionAmount = 5;
+    let questionNo = (questionPage - 1) * questionAmount + 1;
+   
+    
+    //內容資料
+    function tableData() {
+        $.ajax({
+            url: "quizGame.php",
+            data: {
+                type: 'queryQuestion',
+                page: storage['gameQuizPage'],
+                amount: questionAmount
+            },
+            dataType: "text",
+            success: function (response) {
+                questionRow = JSON.parse(response);
+                question = $('.questionRow').clone();
+                pageBox = $('.page-item:first').clone();
+                $('.template').remove();
+                $('tbody').html('');
+                $('.page-item').not('.page-item:first').not('.page-item:last').remove();
+                for (let i = 0; i <= Math.floor((questionRow[1].count - 1) / questionAmount);i++){
+                    pageBox.clone().insertBefore('.page-item:last').find('a').text(i+1);
+                }                    
+                questionRow[0].forEach(element => {
+                    for (key in element) {
+                        if (key == "question_status") {
+                            question.find(`.${key}`).prop('value', element[key]);
+                        } else {
+                            question.find(`.${key}`).text(element[key]);
+                        }
+                    }
+                    question.clone().appendTo('tbody');
+                    questionNo++;
+                });
+                $('.question_status[value="1"]').prop('checked', true);
+                $('.qustionAmount').text(`共 ${questionRow[1].count} 題`)
+                activePage();
+            }
+        })
+    }
+    tableData();
+    
+    //頁面
+    function activePage() {
+        $('.page-item').removeClass('active');
+        $(`.page-item:eq(${storage['gameQuizPage']})`).addClass('active');
+        $('.page-item').click(function (e) {
+            let page = $(this).index();
+            let last = $('.page-item:last').index();
 
-function quizGameInit(){
-    $.ajax({
-        url:"quizGame.php",
-        data:{},
-        dataType:"text",
-        success:function(response){            
-            questionRow = JSON.parse(response);
-            question = $('.questionRow').clone();
-            questionRow.forEach( element => {
-                for ( key in element) {
-                    if (key == "question_status"){
-                        question.find(`.${key}`).prop('value', element[key]);
-                    }else{
-                        question.find(`.${key}`).text(element[key]);
-                    }                    
-                }
-                question.clone().appendTo('tbody');            
-            });
-            $('.question_status[value="1"]').prop('checked',true);
-            $('.questionRow:eq(0)').remove();       
+            if (page == 0 && storage['gameQuizPage'] - 1 > 0) {
+                storage['gameQuizPage']--;
+            } else if (page == last && storage['gameQuizPage'] < last - 1) {
+                // if (storage['gameQuizPage'] + 1 < last)
+                storage['gameQuizPage']++;
+            } else if (page != 0 && page!= last){
+                storage['gameQuizPage'] = page;
+            }
+            console.log(storage['gameQuizPage'], $(this).index())
+
+            // tableData();
+        })
+    }
+    
+    
+
+
+
+    //表單驗證
+    $('#answer').keyup(function () {
+        let answerRow = [];
+        console.log($('.form-control')[2]);
+        for (let i = 0; i < $("input[id*='opt']").length; i++) {
+            answerRow.push($("input[id*='opt']")[i].value);
+        }
+        if (answerRow.indexOf($('#answer').val()) == -1) {
+            $('label[for="answer"] span').css('display', 'inline');
+        } else {
+            $('label[for="answer"] span').css('display', 'none');
         }
     })
+    $('.quizGameConfirm').click(function (e) {
+        if ($('.invalid-feedback').css('display') != 'none') {
+            e.preventDefault();
+        }
+        // e.preventDefault();
+
+        // if ($('.form-control')){
+        //     console.log('no');
+        // }
+        // if($('#answer').val())
+    })
+
+    //燈箱關閉&顯示
+    $('.quizGameCancel').click(function () {
+        $('.questionAddBox').toggleClass('d-none');
+    })
+    $('.quizGameUpload').click(function () {
+        $('.questionAddBox').toggleClass('d-none');
+    })
 }
-window.addEventListener('load',quizGameInit);
+window.addEventListener('load', quizGameInit);
--- dev/back/quizGame.php
+++ dev/back/quizGame.php
@@ -1,13 +1,41 @@
 <?php
 $errMsg = "";
-
-$sql = "select * from game_question";
+$type = $_REQUEST['type'];
+// $type = 'queryQuestion';
 
 
 try {
 // 抓出上架物件
 require_once("../pdoData.php");
-$question = $pdo->query($sql);  
+if($type == 'addQuestion'){
+	$level_no = $_REQUEST['level_no'];
+	$question = $_REQUEST['question'];
+	$opt_1 = $_REQUEST['opt_1'];
+	$opt_2 = $_REQUEST['opt_2'];
+	$opt_3 = $_REQUEST['opt_3'];
+	$opt_4 = $_REQUEST['opt_4'];
+	$answer = $_REQUEST['answer'];
+	$sql = "insert into game_question (level_no ,question ,opt_1 ,opt_2 ,opt_3 ,opt_4 ,answer,question_status) value(:level_no,:question,:opt_1,:opt_2,:opt_3,:opt_4,:answer,1)";
+	$questionAdd = $pdo->prepare($sql);
+	$questionAdd->bindValue(":level_no",$level_no);
+    $questionAdd->bindValue(":question",$question);
+    $questionAdd->bindValue(":opt_1",$opt_1);
+    $questionAdd->bindValue(":opt_2",$opt_2);
+	$questionAdd->bindValue(":opt_3",$opt_3);
+	$questionAdd->bindValue(":opt_4",$opt_4);
+	$questionAdd->bindValue(":answer",$answer);
+	$questionAdd->execute();
+}else if($type == 'queryQuestion'){
+	$amount = $_REQUEST['amount'];
+	// $amount = 10;
+	$questionPage = ($_REQUEST['page']-1)*$amount;
+	// $questionPage = 1;
+	$sql = "select * from game_question limit {$questionPage},{$amount}";
+	$question = $pdo->query($sql);
+	$sql = "select count(*) 'count' from game_question";
+	$count = $pdo->query($sql);
+}
+
 
 } catch (PDOException $e) {
 	$errMsg = $errMsg . "錯誤訊息: " . $e->getMessage() . "</br>";
@@ -16,8 +44,17 @@ $question = $pdo->query($sql);
 if($errMsg !=""){
 	echo "$errMsg";
 }
+if($type == 'addQuestion'){
+	header("Location: quizGame.html");
+} else if($type == 'queryQuestion'){
+	$questionRow = $question->fetchAll(PDO::FETCH_ASSOC);
+	$dataAmount = $count->fetch(PDO::FETCH_ASSOC);
+	$arr = [];
+	$arr[] = $questionRow;
+	$arr[] = $dataAmount;
+	echo json_encode($arr);
+	// echo print_r($arr);
+}
 
-$questionRow = $question->fetchAll(PDO::FETCH_ASSOC);
-echo json_encode($questionRow);
 
 ?>
