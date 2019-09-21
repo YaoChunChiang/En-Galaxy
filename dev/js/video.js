3ae83d030be6cd447e614b89a3096a2a43e3faa1
Alex <t159753g@gmail.com>
2019-09-21 10:40:34 +0800

    add some video

--- dev/js/video.js
+++ dev/js/video.js
@@ -1,21 +1,25 @@
 function videoInit(pageInfo){
+    // pageInfo[0] 影片資訊
+    // pageInfo[1] 影片題目
     console.log(pageInfo)
 
-    $('#videoTitle').html(pageInfo['video_name']);
-    $('#videoType').html(pageInfo['video_type']);
-    $('#videoLevel').html(pageInfo['level_name']);
-    $('#videoSecondTitle').html(pageInfo['video_desc']);
+    $('#videoTitle').html(pageInfo[0]['video_name']);
+    $('#videoType').html(pageInfo[0]['video_type']);
+    $('#videoLevel').html(pageInfo[0]['level_name']);
+    $('#videoSecondTitle').html(pageInfo[0]['video_desc']);
+    $('#video').attr('src', `video/${pageInfo[0]['video_src']}`)
 }
 
-
+//一開始就撈資料
 (function(){
     let url = new URL(window.location.href);
     let sendUrl = url.searchParams.get('video_no');
-    console.log(sendUrl)
-    fetch(`video.php?video_no=${sendUrl}`)
+    console.log(sendUrl);
+    fetch(`video.php?who=init&video_no=${sendUrl}`)
     .then(data => data.json())
     .then(obj => {
-        window.addEventListener('load', function(){videoInit(obj[0])});
+        // console.log(obj)
+        window.addEventListener('load', function(){videoInit(obj)});
     });
 }())//立即執行
 
--- dev/video.php
+++ dev/video.php
@@ -1,12 +1,34 @@
 <?php
     require_once("pdoData.php");
-    $video_no = $_GET['video_no'];
-    $sql = "SELECT vs.subtitles, v.video_no,v.video_name, v.video_desc,v.video_src,v.video_type ,e.level_name 
-            FROM `video_subtitles` vs, video v , eng_level e 
-            WHERE vs.video_no = v.video_no and v.level_no = e.level_no and v.video_no = {$video_no}";
+    $who = $_GET['who'];
 
-    $videoInfo = $pdo->query($sql);
-    $videoInfoRow = $videoInfo->fetchAll(PDO::FETCH_ASSOC);
+    if($who === 'init'){
+        $video_no = $_GET['video_no'];
+        $sql = "SELECT vs.subtitles, v.video_no,v.video_name, v.video_desc,v.video_src,v.video_type ,e.level_name 
+                FROM `video_subtitles` vs, video v , eng_level e 
+                WHERE vs.video_no = v.video_no and v.level_no = e.level_no and v.video_no = :video_no";
     
-    echo json_encode($videoInfoRow);
+        $sqlQuestion = "SELECT video_q, opt_1, opt_2, opt_3, opt_4, answer 
+                        FROM video_qs 
+                        WHERE video_no = :video_no 
+                        and video_q_status = 1";
+    
+        //撈影片資訊
+        $videoInfo = $pdo->prepare($sql);
+        $videoInfo->bindValue(":video_no", $video_no);
+        $videoInfo->execute();
+        $videoInfoRow = $videoInfo->fetchObject();
+        //撈題庫
+        $videoQuestion = $pdo->prepare($sqlQuestion);
+        $videoQuestion->bindValue("video_no", $video_no);
+        $videoQuestion->execute();
+        while($videoQuestionRow = $videoQuestion->fetchObject()){
+            $objs[] = $videoQuestionRow;
+        }
+        // $videoQuestionRow = $videoQuestion->fetchObject();
+    
+        $sendVideoInfo = [$videoInfoRow,$objs];
+    
+        echo json_encode($sendVideoInfo);
+    }
 ?>
