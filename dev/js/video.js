7eab14eec8ee0e6407a9be3369b030c79bc1ff7a
HorseChun <t159753g@gmail.com>
2019-09-21 17:41:04 +0800

    add video

--- dev/js/video.js
+++ dev/js/video.js
@@ -1,4 +1,5 @@
 function videoInit(pageInfo){
+    let memNum = sessionStorage['mem_no']? sessionStorage['mem_no'] : 'notMem';
     // pageInfo[0] 影片資訊
     // pageInfo[1] 影片題目
     console.log(pageInfo)
@@ -8,6 +9,186 @@ function videoInit(pageInfo){
     $('#videoLevel').html(pageInfo[0]['level_name']);
     $('#videoSecondTitle').html(pageInfo[0]['video_desc']);
     $('#video').attr('src', `video/${pageInfo[0]['video_src']}`)
+
+    //製作重點單字
+    function makeImportantVocab(){
+            $(".videoVoc").append();
+
+    // <div class="vocText">
+    //     <div class="vocP">
+    //         <p>1. accord (v.) 一致,符合,和諧,協調;給予,使一致;自願,主動</p>
+    //     </div>
+    //     <div class="addImg">
+    //         <a href="javascript:;"><img src="/img/video/add.png" alt="收藏單字"><span>&nbsp;&nbsp;字卡</span></a>
+    //     </div>
+    // </div>
+    }
+
+
+
+
+
+
+
+    
+    let questionNum = 0; //偵測在第幾題
+    //製作題目的function
+    function makeVideoQuestion(questions, qno = 0){
+        $('.quizList').html(questionNum + 1);
+        // console.log(questions[qno]['opt_1']);
+        $('.quizPage').html(`Page ${qno + 1} of ${questions.length}`);
+        $('#videoQTitle').html(questions[qno]['video_q']);
+        $('.opt_1').html(`<input type="radio" name="myAns" value="0">&nbsp;&nbsp;${questions[qno]['opt_1']}`)
+        $('.opt_2').html(`<input type="radio" name="myAns" value="1">&nbsp;&nbsp;${questions[qno]['opt_2']}`)
+        $('.opt_3').html(`<input type="radio" name="myAns" value="2">&nbsp;&nbsp;${questions[qno]['opt_3']}`)
+        $('.opt_4').html(`<input type="radio" name="myAns" value="3">&nbsp;&nbsp;${questions[qno]['opt_4']}`)
+        // $('.quizAnsAll').append("");
+    }
+    makeVideoQuestion(pageInfo[1]);
+    
+    //檢查答案
+    function checkAns(){
+        console.log(selectedQuest);
+        console.log(pageInfo[1]);
+        let wrongQ = [];
+        let message = '您答錯的題號為: ';
+        pageInfo[1].forEach((obj, i)=> {
+            let arr = [];
+            let answer = obj['answer'];
+            arr.push(obj['opt_1']);
+            arr.push(obj['opt_2']);
+            arr.push(obj['opt_3']);
+            arr.push(obj['opt_4']);
+
+            let rightAnsPos = arr.indexOf(answer);
+            if(parseInt(selectedQuest[i]) === rightAnsPos){
+                // wrongQ.push(true);
+            }else{
+                //答錯的題號放入陣列
+                wrongQ.push(i + 1);
+            }
+            // console.log(arr.indexOf(answer));
+        });
+        message += wrongQ.join(", ");
+        if(wrongQ.length === 0){
+            if(memNum === 'notMem'){
+                alertBoxShow("恭喜你全對</br>會員才可以獲得獎金哦", '恭喜', 'green',()=>{
+                    $('#loginBox').css('display', 'block');
+                });
+            }else{
+                $.post('video.php', {who: 'addMoney', memNum});
+                alertBoxShow("恭喜你全對</br>獲得G幣10元", '恭喜', 'green',()=>{
+                    // 會員才加錢
+                    window.location.href = "videoList.html";
+                });
+            }
+            
+        }else{
+            if(memNum === 'notMem'){
+                alertBoxShow(message + '</br>可登入會員進行學習呦', '繼續加油', 'green',()=>{
+                    $('#loginBox').css('display', 'block');
+                })
+            }else{
+                alertBoxShow(message + '</br>可去單字或遊戲頁面繼續學習呦', '繼續加油', 'green', ()=>{
+                    window.scrollTo(0,0);
+                    location.reload();
+                })
+            }
+        }
+    }
+
+
+    // 按下一題
+    let selectedQuest = [];
+    function nextQuiz(){
+        
+        if($('input:radio:checked[name="myAns"]').val() != undefined){
+            // 玩家選的選項
+            selectedQuest[questionNum] = $('input:radio:checked[name="myAns"]').val();
+            // console.log((questionNum + 2) / pageInfo[1].length * 100);
+            
+            //移動進度條
+            $('.progress-wrap').data('progress-percent', (questionNum + 2) / pageInfo[1].length * 100);
+            moveProgressBar();
+
+            // //更新進度數字
+            // $('.quizPage').html(`Page ${questionNum + 1}} of ${pageInfo[1].length}`);
+
+
+            if( $('.quizNext').html() === '送出'){
+                checkAns();
+                // alertBoxShow('WEEEEEEEEEEEEEE');
+                
+            }else{
+                if(questionNum < pageInfo[1].length -1 ){
+                    questionNum++;
+                    $('.quizList').html(questionNum + 1);
+                    makeVideoQuestion(pageInfo[1], questionNum);
+                    //如果按上一題後又按下一題會記得之前的選擇
+                    if(selectedQuest[questionNum]){
+                        $('.quizAns input[name="myAns"]')[selectedQuest[questionNum]].setAttribute('checked','ckecked');
+                    }
+                }
+                if(questionNum == pageInfo[1].length - 1){
+                    $('.quizNext').html('送出');
+                }
+            }
+
+        }else{
+            alertBoxShow('必須填入回答');
+        }
+    }
+    // 按上一題
+    function preQuiz(){
+        console.log(selectedQuest);
+        if(questionNum != 0){
+            //移動進度條
+            console.log((questionNum + 2) / pageInfo[1].length * 100);
+            $('.progress-wrap').data('progress-percent', (questionNum) / pageInfo[1].length * 100);
+            moveProgressBar();
+
+
+
+            questionNum--;
+            $('.quizList').html(questionNum + 1);
+            makeVideoQuestion(pageInfo[1], questionNum);
+
+
+            // console.log()
+            // console.log($('.quizAns input[name="myAns"]')[selectedQuest[questionNum]])
+            //按上一題會記得之前的選擇
+            $('.quizAns input[name="myAns"]')[selectedQuest[questionNum]].setAttribute('checked','ckecked');
+            $('.quizNext').html('下一題');
+        }
+    }
+
+    moveProgressBar();
+    // on browser resize...
+    $(window).resize(function() {
+        moveProgressBar();
+    });
+
+    // SIGNATURE PROGRESS
+    function moveProgressBar() {
+        // console.log($('.progress-wrap').data('progress-percent'));
+        var getPercent = ($('.progress-wrap').data('progress-percent') / 100);
+        var getProgressWrapWidth = $('.progress-wrap').width();
+        var progressTotal = getPercent * getProgressWrapWidth;
+        var animationLength = 2500;
+        
+        // on page load, animate percentage bar to data percentage length
+        // .stop() used to prevent animation queueing
+        $('.progress-bar').stop().animate({
+            left: progressTotal
+        }, animationLength);
+    }
+
+
+    $('.quizNext').click(nextQuiz);
+    $('.quizPre').click(preQuiz);
+
+    $(".videoTestGame").click(()=>{location.href = 'game.html'});
+    $(".videoTestCard").click(()=>{location.href = 'card.html'});
 }
 
 //一開始就撈資料
