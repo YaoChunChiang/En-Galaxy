5fce924ef343ef1b78b4e7d8ac8b7dd6f55b612d
HorseChun <t159753g@gmail.com>
2019-09-26 16:44:10 +0800

    交給你了

--- dev/home.php
+++ dev/home.php
@@ -3,12 +3,21 @@ $errMsg = "";
 try{
     require_once("pdoData.php");
 
+    $who = $_REQUEST['who'];
 
+    if($who === 'getQuestion'){
+        $sql = 'SELECT * FROM `member_question` limit 1, 3';
+        $questions = $pdo->query($sql);
+        $questionRows = $questions->fetchAll(PDO::FETCH_ASSOC);
 
+        echo json_encode($questionRows);
+    }else if($who === 'getActivityData'){
+        $sql = 'SELECT * FROM `activity` a, `mem_main` m where a.mem_no = m.mem_no limit 1, 3';
+        $activitys = $pdo->query($sql);
+        $activityRows = $activitys->fetchAll(PDO::FETCH_ASSOC);
 
-
-
-
+        echo json_encode($activityRows);
+    }
 
 
 
