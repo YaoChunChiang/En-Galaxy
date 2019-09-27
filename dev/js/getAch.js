f0f745fc207051bbe90d7062ed90a9df0be22efb
alex1290 <s101242006@mail1.ncnu.edu.tw>
2019-09-28 03:11:19 +0800

    fix

--- dev/js/getAch.js
+++ dev/js/getAch.js
@@ -9,21 +9,25 @@ function achGet(ach_no) {
     });
 }
 function achInit() {
-    let locationPath = location.pathname;
-    if (locationPath.indexOf('/game.html') != -1) {
-        $('.gameMenuPlay').click(function(){
-            achGet(1)
-        })
-    }else if(locationPath.indexOf('/card.html') != -1){
-        // achGet(2)
-    }else if(locationPath.indexOf('/role.html') != -1){
-        $('.purchaseBtn').click(function(){
-            achGet(3)
-        })
-    }else if(locationPath.indexOf('/video.html?video_no') != -1){
-        $('#favorateVideoAdd').click(function(){
-            achGet(4)
-        })
+    if (sessionStorage['mem_no'] != null) {
+        let locationPath = location.pathname;
+        if (locationPath.indexOf('/game.html') != -1) {
+            $('.gameMenuPlay').click(function () {
+                achGet(1)
+            })
+        } else if (locationPath.indexOf('/card.html') != -1) {
+            // achGet(2)
+        } else if (locationPath.indexOf('/role.html') != -1) {
+            $('.purchaseBtn').click(function () {
+                achGet(3)
+            })
+        } else if (locationPath.indexOf('/video.html?video_no') != -1) {
+            $('#favorateVideoAdd').click(function () {
+                achGet(4)
+            })
+        }
     }
 }
-window.addEventListener('load',achInit,false)
+window.addEventListener('load', achInit, false)
+
+
