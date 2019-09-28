0f9d02186c591ee3a2c92ba480e428b65f179a74
whnliao <whnliao@gmail.com>
2019-09-28 12:13:59 +0800

    account fix

--- dev/back/accountManage.php
+++ dev/back/accountManage.php
@@ -1,59 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-  <title>En-Galaxy後台</title>
-  <!-- Icons-->
-  <link href="node/@coreui/icons/css/coreui-icons.min.css" rel="stylesheet">
-  <link href="node/flag-icon-css/css/flag-icon.min.css" rel="stylesheet">
-  <link href="node/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-  <link href="node/simple-line-icons/css/simple-line-icons.css" rel="stylesheet">
-  <!-- Main styles for this application-->
-  <link href="css/style.css" rel="stylesheet">
-</head>
-
-<body class="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show">
-  <!-- top_header -->
-  @@include('template/top_header.html')
-  <div class="app-body">
-    <div class="sidebar">
-      <!-- sidebar menu-->
-      @@include('template/sidebar_nav.html')
-      <button class="sidebar-minimizer brand-minimizer" type="button"></button>
-    </div>
-    <main class="main">
-      <!-- Breadcrumb-->
-      <ol class="breadcrumb">
-        <li class="breadcrumb-item">Home</li>
-        <li class="breadcrumb-item">
-          <a href="#">Admin</a>
-        </li>
-        <li class="breadcrumb-item active">Dashboard</li>
-        <!-- Breadcrumb Menu-->
-        <li class="breadcrumb-menu d-md-down-none">
-          <div class="btn-group" role="group" aria-label="Button group">
-            <a class="btn" href="#">
-              <i class="icon-speech"></i>
-            </a>
-            <a class="btn" href="./">
-              <i class="icon-graph"></i>  Dashboard</a>
-            <a class="btn" href="#">
-              <i class="icon-settings"></i>  Settings</a>
-          </div>
-        </li>
-      </ol>
-        <!-- 中間內容 -->
-        @@include('template/accountManageContent.php') 
-        <!-- end -->
-    </main>
-
-  </div>
-  @@include('template/footer.html')
-  <!-- CoreUI and necessary plugins-->
-  @@include('template/script.html')
-</body>
-
-</html>
+<?php
+try {
+    require_once("../pdoData.php");
+    $action = $_REQUEST["action"];
+    if ($action == "loadAccount") {
+        $sql = "select * from mem_main";
+        $memberMain = $pdo->prepare($sql);
+        $memberMain->execute();
+        if ($memberMain->rowCount() == 0) {
+            echo "{}";
+        } else {
+            $memberMainRows = $memberMain->fetchAll();
+            echo json_encode($memberMainRows);
+        }
+    } else if ($action == "changeAuthority") {
+        $mem_no = $_POST["mem_no"];
+        $mem_now = $_POST["check"];
+        if($mem_now == 'true'){
+            $mem_status = 1;
+        }
+        if($mem_now  == 'false'){
+            $mem_status = 0;
+        }
+        // exit($mem_status);
+        $memberChangeSql = " update mem_main set mem_status = '$mem_status' where mem_no = '$mem_no'";
+        $memberChange = $pdo->prepare($memberChangeSql);
+        // $memberChange->bindValue(":mem_no", $mem_no);
+        // $memberChange->bindValue(":mem_status", $mem_status);
+        $memberChange->execute();
+        if ($memberChange->rowCount() == 0) {
+            echo "{}";
+        } else {
+            $memberChangeRows = $memberChange->fetchAll();
+            echo json_encode($memberChangeRows);
+        }
+    }
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
